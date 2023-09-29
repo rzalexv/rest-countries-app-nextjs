d93a99507688bfc4f244c89a01e545dd9d3a66fb
rzalexv <alexanderrdev@gmail.com>
2023-09-29 16:23:58 -0500

    fix: Flag support for chrome

--- src/components/CountryBorder.tsx
+++ src/components/CountryBorder.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 
 import { getCountryByCode } from "@/utils";
 
@@ -14,7 +15,7 @@ export const CountryBorder: FC<Props> = async ({ border }) => {
 
   return (
     <Link href={`/country/${country.cca3}`} className="inline-flex text-sm gap-2 items-center shadow-lg bg-white rounded-md py-1 px-3 transition-colors duration-300 hover:bg-neutral-200 dark:bg-dark-soft dark:text-white dark:hover:bg-slate-700">
-      <span className="text-2xl">{country.flag}</span> {country.name.common}
+      <Image src={country.flags.png} alt={country.flags.alt ?? `Flag of ${country.name.common}`} width={24} height={24} /> {country.name.common}
     </Link>
   );
 };
